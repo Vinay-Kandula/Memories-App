7e86c75fd8418a1437e5cf7a37ffafe78c747799
Vinay Kandula <vinaykandula321@gmail.com>
2024-01-07 12:14:45 +0530

    th commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 const corsOptions ={
-    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
